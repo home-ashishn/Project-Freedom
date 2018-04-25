2b81d7823422e7f81e8e234bd0626785b035ce49
home-ashishn <home-ashishn@users.noreply.github.com>
2018-04-25 23:09:22 +0530

    till now + daily data

--- databases/Misc Queries/LOAD-EXCEL-QUERY.sql
+++ databases/Misc Queries/LOAD-EXCEL-QUERY.sql
@@ -3,7 +3,7 @@
 -- set @date_ref_txt = '05-03-2018';
 delete from basis_for_calls;
 
-LOAD DATA LOCAL INFILE 'D:\\Project-Freedom\\databases\\Daily Data\\09-03-2018\\basis-for-calls.csv' 
+LOAD DATA LOCAL INFILE 'C:\\Users\\capiot\\Documents\\GitHub\\Project-Freedom\\databases\\Daily Data\\06-03-2018\\basis-for-calls.csv' 
 INTO TABLE basis_for_calls
 		FIELDS TERMINATED BY ','
         OPTIONALLY ENCLOSED BY '"'
@@ -13,7 +13,7 @@ update basis_for_calls set confidence_level = confidence_level + 7.5;
 
 delete from selected_instrument;
 
-LOAD DATA LOCAL INFILE 'D:\\Project-Freedom\\databases\\Daily Data\\09-03-2018\\selected-instrument.csv' 
+LOAD DATA LOCAL INFILE 'C:\\Users\\capiot\\Documents\\GitHub\\Project-Freedom\\databases\\Daily Data\\06-03-2018\\selected-instrument.csv' 
 INTO TABLE selected_instrument
 		FIELDS TERMINATED BY ','
         OPTIONALLY ENCLOSED BY '"'
@@ -35,19 +35,20 @@ expiry_date_full = default;
 
 
 
+delete from live_data;
 
-/*
-LOAD DATA LOCAL INFILE 'D:\\Project-Freedom\\databases\\Daily Data\\20-02-2018\\live-stock-data.csv' 
+LOAD DATA LOCAL INFILE 'C:\\Users\\capiot\\Documents\\GitHub\\Project-Freedom\\databases\\Daily Data\\06-03-2018\\live-stock-data.csv' 
 INTO TABLE live_data
 		FIELDS TERMINATED BY ','
         OPTIONALLY ENCLOSED BY '"'
 IGNORE 1 LINES;
 
 
+delete from live_option_price_data;
 
-LOAD DATA LOCAL INFILE 'D:\\Project-Freedom\\databases\\Daily Data\\02-02-2018\\live-option-data.csv' 
+LOAD DATA LOCAL INFILE 'C:\\Users\\capiot\\Documents\\GitHub\\Project-Freedom\\databases\\Daily Data\\06-03-2018\\live-option-data.csv' 
 INTO TABLE live_option_price_data
 		FIELDS TERMINATED BY ','
         OPTIONALLY ENCLOSED BY '"'
 IGNORE 1 LINES;
-*/
+
