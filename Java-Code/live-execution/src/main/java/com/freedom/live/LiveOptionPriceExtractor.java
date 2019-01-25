38e57d03ccb4e99691e44e416c4af5c7cff5dec4
home-ashishn <home-ashishn@users.noreply.github.com>
2019-01-25 11:34:09 +0530

    Revert "till now"
    This reverts commit 32953d2b13d323877b79e7cf5c0bcafe57695909.

--- Java-Code/live-execution/src/main/java/com/freedom/live/LiveOptionPriceExtractor.java
+++ Java-Code/live-execution/src/main/java/com/freedom/live/LiveOptionPriceExtractor.java
@@ -247,7 +247,6 @@ public class LiveOptionPriceExtractor {
 				
 				DateTime currentTime = DateTime.now();
 				
-				/*
 				System.out.println("444444 555555555 , currentTime zone = "
 				+currentTime.getZone());
 
@@ -261,8 +260,8 @@ public class LiveOptionPriceExtractor {
 					currentTime = currentTime.plusMinutes(30);
 				}
 				
-*/
-//				System.out.println("77777 88888888 for IST, currentTime = "+currentTime);
+
+				System.out.println("77777 88888888 for IST, currentTime = "+currentTime);
 				
 				liveOptionPriceData.setCurr_time(currentTime);
 				liveOptionPriceData.setSymbol(selectedInstrument.getSymbol());
@@ -363,7 +362,7 @@ public class LiveOptionPriceExtractor {
 	
 	private void sop(String text) {
 
-		//System.out.println(text);
+		System.out.println(text);
 	}
 
 
